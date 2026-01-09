fe78d91d0a0f3d6acbe43cccd8c6231755659cd7
WebArtSoham <soham.paul@webart.technology>
2026-01-09 11:30:09 +0530

    Form structure & functionality complete

---
+++ assets/custom.js
@@ -0,0 +1,9 @@
+// $(document).ready(function() {
+//     $('#customer_callDateTime').datetimepicker({
+//         format: 'Y-m-d H:i',
+//         minDate: 0,
+//         step: 30,
+//         dayOfWeekStart: 1,
+//         scrollInput: false
+//     });
+// });
